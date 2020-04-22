60ba4835a933f0fbc8f616ad0851e240d9d97858
Agustin Jacobsen <jacobsen816@gmail.com>
2020-04-21 21:54:43 -0300

    Partido y prueba de solucion

---
+++ 1 - Ejercicio Futbol/Logica/TorneoDeFutbol.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class TorneoDeFutbol
+    {
+        public List<Partido> Partidos { get; set; }
+
+        public bool NuevoPartido(Arbitro arbitro, string ciudad, Equipo equipoLocal, Equipo equipoVisitante)
+        {
+            List<Arbitro> arbitros = new List<Arbitro>();
+            arbitros.Add(arbitro);
+
+            Partido nuevoPartido = new Partido()
+            {
+                Arbitros = arbitros,
+                Ciudad = ciudad,
+                EquipoLocal = equipoLocal,
+                EquipoVisitante = equipoVisitante
+            };
+
+            this.Partidos = new List<Partido>();
+
+            this.Partidos.Add(nuevoPartido);
+
+            return true;
+        }
+    }
+}
